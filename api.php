4015cd9714ba8867f0cab35d53045175731d86ca
Andres Felipe Gomez Maya <feliphegomez@gmail.com>
2018-07-10 17:40:24 -0500

    Update api.php

--- api.php
+++ api.php
@@ -2714,8 +2714,8 @@ class PHP_CRUD_API {
  $api = new PHP_CRUD_API(array(
  	'dbengine'=>'MySQL',
  	'hostname'=>'10.1.1.108',
- 	'username'=>'fg',
- 	'password'=>'1035429360',
+ 	'username'=>'db_emtelco',
+ 	'password'=>'db_emtelco9510*',
  	'database'=>'',
  	'charset'=>'utf8mb4'
  ));
